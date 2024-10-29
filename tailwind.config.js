323d67c08a4b914b9441b1326990314a68aedde6
Ibrahim Nidam <ibrahim.nidam22@gmail.com>
2024-10-29 10:33:42 +0100

    Finished setting up tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.html",              // Main HTML file in the project root
+    "./assets/js/**/*.{js}"   // All JavaScript files inside assets/js folder
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
